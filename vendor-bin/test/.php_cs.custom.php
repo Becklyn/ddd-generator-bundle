91eb76ba81b1ae869d9f4306ff3af07c65af1d96
Pascal Gläßer <pg@becklyn.com>
2021-08-26 12:24:08 +0200

    Use custom php_cs config to prevent DddEntityTestMaker classes to be marked as @internal and final

---
+++ vendor-bin/test/.php_cs.custom.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+$config = include "vendor/becklyn/php-cs/.php_cs.dist.php";
+
+$config->setRules([
+    // This override will prevent that the DddEntityTestMaker classes are marked as @internal and final
+    "php_unit_internal_class" => false,
+]);
+
+return $config;
