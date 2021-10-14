a15ba4689de62aed910728d3ed55bbdd1b2de7f8
Samuel Oechsler <so@becklyn.com>
2021-10-14 11:45:34 +0200

    Add entitiy pre-fill to command handler
    Now the respective entity is pre-filled into the handle method of the command handler
    
    # Conflicts:
    #	src/Resources/skeleton/ddd/command-handler.tpl.php

--- src/Resources/skeleton/ddd/command-handler.tpl.php
+++ src/Resources/skeleton/ddd/command-handler.tpl.php
@@ -13,6 +13,8 @@ use Becklyn\Ddd\Events\Domain\EventProvider;
 class <?= $class_name; ?> extends CommandHandler
 {
     public function __construct (
+        private <?= $entity; ?>Repository $<?= $strtocamel($entity); ?>Repository,
+
         // TODO inject dependencies into <?= $class_name; ?>::__construct
     ) {}
 
@@ -26,6 +28,10 @@ class <?= $class_name; ?> extends CommandHandler
      */
     protected function execute ($command) : ?EventProvider
     {
+        $<?= $strtocamel($entity); ?> = $this-><?= $strtocamel($entity); ?>Repository->findOneById($command-><?= $strtocamel($entity); ?>Id());
+
         // TODO implement <?= $class_name; ?>::execute
+
+        return $<?= $strtocamel($entity); ?>;
     }
 }
